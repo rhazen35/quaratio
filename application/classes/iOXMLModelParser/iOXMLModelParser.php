3a0c8aae1b5e564d65c998b13e740b798945e761
rhazen35 <rhazen35@gmail.com>
2016-07-14 18:18:46 +0200

    xml parser can read most of the file, only connectors and links remaining

--- application/classes/iOXMLModelParser/iOXMLModelParser.php
+++ application/classes/iOXMLModelParser/iOXMLModelParser.php
@@ -78,7 +78,11 @@ if( !class_exists( "IOXMLModelParser" ) ):
 
             foreach ( $elements as $element ):
 
+                /** @var  $namespaces */
                 $namespaces = $element->getNameSpaces( true );
+
+                /** Class name */
+                $className = ( new xmlController\XmlController( $element, "getNodeAttribute", "name" ) )->request();
 
                 /**
                  * Get the element properties to access the isRoot value
@@ -87,100 +91,138 @@ if( !class_exists( "IOXMLModelParser" ) ):
 
                 foreach ( $properties as $property ):
 
-                    /**
-                     * Get the isRoot, this class will be shown first.
-                     * The root also defines the base which the template generator will request
-                     */
-                    $isRoot = ( new xmlController\XmlController( $property, "getNodeAttribute", "isRoot" ) )->request();
-
-                    /**
-                     * Detailed class information
-                     */
-
-                    /** Class name */
-                    $className = ( new xmlController\XmlController( $element, "getNodeAttribute", "name" ) )->request();
-
-                    if ($isRoot === "true"):
-                        $classArray[$className]['Root'] = "true";
-                    else:
-                        $classArray[$className]['Root'] = "false";
-                    endif;
-
-                    /** @var  $abstract */
-                    $abstract = ( new xmlController\XmlController( $property, "getNodeAttribute", "isAbstract" ) )->request();
-
-                    /**
-                     * Get the namespace attributes from the class
-                     *
-                     * @var  $xmiNamespace
-                     * @var  $type
-                     * @var  $idref
-                     */
-                    $xmiNamespace = $namespaces['xmi'];
-                    $type         = (string) $element->attributes( $xmiNamespace )->type;
-                    $idref        = (string) $element->attributes( $xmiNamespace )->idref;
-
-                    /** Namespaced type and idref to class array*/
-                    $classArray[$className]['type']  = $type;
-                    $classArray[$className]['idref'] = $idref;
-
-                    /**
-                     * Non-namespaced attributes
-                     * Scope, documentation and abstract to class array
-                     */
-                    $classArray[$className]['scope']         = ( new xmlController\XmlController( $element, "getNodeAttribute", "scope" ) )->request();
-                    $classArray[$className]['documentation'] = ( new xmlController\XmlController( $property, "getNodeAttribute", "documentation" ) )->request();
-                    $classArray[$className]['abstract']      = $abstract;
-
-                    /**
-                     * Get the operations of the class.
-                     *
-                     * Operations are calculator calculations.
-                     * The input data will be added to match the collected excel cells
-                     *
-                     * @var  $operations
-                     * @var  $totalOperations
-                     */
-
-                    $operations      = $element->children()->operations;
-                    $totalOperations = count( $operations->operation );
-
-                    /** Create an array with the operation name and add it to the operations array */
-
-                    for ($i = 0; $i < $totalOperations; $i++):
-
-                        $operationName = (string) $operations->operation[$i]->attributes()->name;
-                        $classArray[$className]['operations'][$operationName] = array();
-
-                    endfor;
-
-                    /**
-                     * Input fields and enumeration/data type fields
-                     *
-                     * - Data types of fields can be found under their own class in the xml model
-                     */
-
-                    $attributes      = $element->children()->attributes;
-                    $totalAttributes = count( $attributes->attribute );
-
-                    /** Loop trough the attributes of the class */
-                    for ($i = 0; $i < $totalAttributes; $i++):
-
-                        /** Input field names /enumeration/data type names */
-                        $inputName = (string) $attributes->attribute[$i]->attributes()->name;
-                        $classArray[$className]['fields'][$inputName] = array();
-
-                        /** input field documentation */
-                        $inputFieldDocumentation = (string) $attributes->attribute[$i]->documentation->attributes()->value;
-                        $classArray[$className]['fields'][$inputName]['documentation'] = $inputFieldDocumentation;
-
-                        /** Input field data type */
-                        $inputFieldDataType = (string) $attributes->attribute[$i]->properties->attributes()->type;
-                        $classArray[$className]['fields'][$inputName]['data_type'] = $inputFieldDataType;
-
-                    endfor;
+                    $property = $property;
 
                 endforeach;
+
+                /**
+                 * Get the isRoot, this class will be shown first.
+                 * The root also defines the base which the template generator will request
+                 */
+                $isRoot = ( new xmlController\XmlController( $property, "getNodeAttribute", "isRoot" ) )->request();
+
+                /**
+                 * Detailed class information
+                 */
+
+                if ($isRoot === "true"):
+                    $classArray[$className]['Root'] = "true";
+                else:
+                    $classArray[$className]['Root'] = "false";
+                endif;
+
+                /**
+                 * Get the model node and add to the model node array
+                 */
+
+                $modelNode = $element->children()->model;
+                $classArray[$className]['package']  = ( new xmlController\XmlController( $modelNode, "getNodeAttribute", "package" ) )->request();
+                $classArray[$className]['package2'] = ( new xmlController\XmlController( $modelNode, "getNodeAttribute", "package2" ) )->request();
+
+                /** @var  $abstract */
+                $abstract = ( new xmlController\XmlController( $property, "getNodeAttribute", "isAbstract" ) )->request();
+
+                /**
+                 * Get the namespace attributes from the class
+                 *
+                 * @var  $xmiNamespace
+                 * @var  $type
+                 * @var  $idref
+                 */
+                $xmiNamespace = $namespaces['xmi'];
+                $type         = (string) $element->attributes( $xmiNamespace )->type;
+                $idref        = (string) $element->attributes( $xmiNamespace )->idref;
+
+                /** Namespaced type and idref to class array*/
+                $classArray[$className]['type']  = $type;
+                $classArray[$className]['idref'] = $idref;
+
+                /**
+                 * Non-namespaced attributes
+                 * Scope, documentation and abstract to class array
+                 */
+                $classArray[$className]['scope']         = ( new xmlController\XmlController( $element, "getNodeAttribute", "scope" ) )->request();
+                $classArray[$className]['documentation'] = ( new xmlController\XmlController( $property, "getNodeAttribute", "documentation" ) )->request();
+                $classArray[$className]['abstract']      = $abstract;
+
+                /**
+                 * Get the operations of the class.
+                 *
+                 * Operations are calculator calculations.
+                 * The input data will be added to match the collected excel cells
+                 *
+                 * @var  $operations
+                 * @var  $totalOperations
+                 */
+
+                $operations      = $element->children()->operations;
+                $totalOperations = count( $operations->operation );
+
+                /** Create an array with the operation name and add it to the operations array */
+
+                for ($i = 0; $i < $totalOperations; $i++):
+
+                    $operationName  = (string) $operations->operation[$i]->attributes()->name;
+                    $idref          = (string) $operations->operation[$i]->attributes( $xmiNamespace )->idref;
+                    $position       = (string) $operations->operation[$i]->properties->attributes()->position;
+                    $type           = (string) $operations->operation[$i]->type->attributes()->type;
+                    $abstract       = (string) $operations->operation[$i]->type->attributes()->isAbstract;
+
+                    $behaviour      =  (string) $operations->operation[$i]->behaviour->attributes()->value;
+                    $behaviour      = "<?xml version='1.0' encoding='UTF-8'?><calculated>".$behaviour."</calculated>";
+
+                    $documentation  = (string) $operations->operation[$i]->documentation->attributes()->value;
+
+                    $classArray[$className]['operations'][$operationName] = array();
+
+                    $classArray[$className]['operations'][$operationName]['idref']          = $idref;
+                    $classArray[$className]['operations'][$operationName]['position']       = $position;
+                    $classArray[$className]['operations'][$operationName]['type']           = $type;
+                    $classArray[$className]['operations'][$operationName]['abstract']       = $abstract;
+                    $classArray[$className]['operations'][$operationName]['documentation']  = $documentation;
+
+                    $xml = simplexml_load_string($behaviour);
+                    $totalFiles = count($xml->excel);
+
+                    if(!empty($totalFiles)):
+
+                        for ($k = 0; $k < $totalFiles; $k++):
+                            $classArray[$className]['operations'][$operationName]['excel'.($k+1)]['file']  = (string) $xml->excel[$k]->file;
+                            $classArray[$className]['operations'][$operationName]['excel'.($k+1)]['tab']   = (string) $xml->excel[$k]->tab;
+                            $classArray[$className]['operations'][$operationName]['excel'.($k+1)]['cells'] = (string) $xml->excel[$k]->cells;
+                        endfor;
+
+                    endif;
+
+                    //$classArray[$className]['operations'][$operationName]['behaviour']      = $behaviour;
+
+                endfor;
+
+                /**
+                 * Input fields and enumeration/data type fields
+                 *
+                 * - Data types of fields can be found under their own class in the xml model
+                 */
+
+                $attributes      = $element->children()->attributes;
+                $totalAttributes = count( $attributes->attribute );
+
+                /** Loop trough the attributes of the class */
+                for ($i = 0; $i < $totalAttributes; $i++):
+
+                    /** Input field names /enumeration/data type names */
+                    $inputName = (string) $attributes->attribute[$i]->attributes()->name;
+                    $classArray[$className]['fields'][$inputName] = array();
+
+                    /** input field documentation */
+                    $inputFieldDocumentation = (string) $attributes->attribute[$i]->documentation->attributes()->value;
+                    $classArray[$className]['fields'][$inputName]['documentation'] = $inputFieldDocumentation;
+
+                    /** Input field data type */
+                    $inputFieldDataType = (string) $attributes->attribute[$i]->properties->attributes()->type;
+                    $classArray[$className]['fields'][$inputName]['data_type'] = $inputFieldDataType;
+
+                endfor;
 
             endforeach;
 
